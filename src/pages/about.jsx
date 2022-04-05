3f5213f2c8f17de6cd963a0266376afc5d78675e
AliveKiwi <prateekjais.dev@gmail.com>
2022-04-05 19:46:16 +0530

    modified about.jsx and index.jsx to use component in layout.jsx

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,14 +1,11 @@
 import React from "react";
-import { Link } from 'gatsby'
+import Layout from "../components/layout";
 
 const AboutPage = () => {
     return (
-        <main>
-            <title>About Me</title>
-            <h1>About Me</h1>
-            <Link to="/">Home</Link>
+        <Layout pageTitle="About Me">
             <p>Hi, my name is AliveKiwi</p>
-        </main>
+        </Layout>
     )
 }
 

f0baf6dcf9fddd5c1a833ec5fbde7b638db6e80d
AliveKiwi <prateekjais.dev@gmail.com>
2022-04-05 17:23:20 +0530

    first edit to default files

---
+++ src/pages/about.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AboutPage = () => {
+    return (
+        <main>
+            <div>Hi, my name is AliveKiwi</div>
+        </main>
+    )
+}
+
+export default AboutPage
---
+++ src/pages/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const IndexPage = () => {
+  return (
+    <main>
+      <title>Home Page</title>
+      <h1>Welcome to my Gatsby site!</h1>
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+    </main>
+  );
+};
+export default IndexPage;

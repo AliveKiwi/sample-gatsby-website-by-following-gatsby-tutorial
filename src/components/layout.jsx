6798b76d5e8c07772a27d53a59ec6c29b8dcf3e4
AliveKiwi <prateekjais.dev@gmail.com>
2022-04-10 19:09:49 +0530

    added blog page and Finished Gatsby Tutorial Part 4

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,16 +1,31 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
+import { Link, useStaticQuery, graphql } from 'gatsby';
 import {
   container,
   heading,
   navLinks,
   navLinkItem,
   navLinkText,
+  siteTitle,
 } from './layout.module.css';
 
 const Layout = ({ pageTitle, children }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+
   return (
     <div className={container}>
+      <title>
+        {pageTitle} | {data.site.siteMetadata.title}
+      </title>
+      <header className={siteTitle}>{data.site.siteMetadata.title}</header>
       <title>{pageTitle}</title>
       <nav>
         <ul className={navLinks}>
@@ -22,6 +37,11 @@ const Layout = ({ pageTitle, children }) => {
           <li className={navLinkItem}>
             <Link className={navLinkText} to="/about">
               About
+            </Link>
+          </li>
+          <li className={navLinkItem}>
+            <Link className={navLinkText} to="/blog">
+              Blog
             </Link>
           </li>
         </ul>
---
+++ src/pages/blog.jsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import { graphql } from 'gatsby';
+import Layout from '../components/layout';
+
+const BlogPage = ({ data }) => {
+  return (
+    <Layout pageTitle="My Blog Posts">
+      <ul>
+        {data.allFile.nodes.map((node) => (
+          <li key={node.name}>{node.name}</li>
+        ))}
+      </ul>
+    </Layout>
+  );
+};
+
+export const query = graphql`
+  query {
+    allFile(filter: { sourceInstanceName: { eq: "blog" } }) {
+      nodes {
+        name
+      }
+    }
+  }
+`;
+export default BlogPage;

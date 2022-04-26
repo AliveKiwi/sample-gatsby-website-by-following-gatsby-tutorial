9c6f78f20d75b2740a97db6f87dae557945fae70
AliveKiwi <prateekjais.dev@gmail.com>
2022-04-26 18:34:13 +0530

    Finished Gatsby Tutorial Part 6

---
+++ src/pages/blog/index.jsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import { Link, graphql } from 'gatsby';
+import { MDXRenderer } from 'gatsby-plugin-mdx';
+import Layout from '../../components/layout';
+
+const BlogPage = ({ data }) => {
+  return (
+    <Layout pageTitle="My Blog Posts">
+      {data.allMdx.nodes.map((node) => (
+        <article key={node.id}>
+          <h2>
+            <Link to={`/blog/${node.slug}`}>{node.frontmatter.title}</Link>
+          </h2>
+          <p>Posted: {node.frontmatter.date}</p>
+          {/* <MDXRenderer>{node.body}</MDXRenderer> */}
+        </article>
+      ))}
+    </Layout>
+  );
+};
+
+export const query = graphql`
+  query {
+    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+      nodes {
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+        }
+        id
+        slug
+      }
+    }
+  }
+`;
+
+export default BlogPage;
